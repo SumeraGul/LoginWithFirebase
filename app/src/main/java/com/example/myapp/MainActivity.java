cf5623b3da260fdf8d0c64c1f90be300aae2b0e0
sumera <sumeragul110@gmail.com>
2024-03-18 14:17:09 +0500

    Ist commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.myapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    Button btnregister;
+    TextInputLayout inputemail, inputpasword;
+    EditText edtemail, editpassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        edtemail = findViewById(R.id.etmail);
+        editpassword = findViewById(R.id.etPassword);
+        btnregister = findViewById(R.id.btnregister);
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser != null){
+            Intent i = new Intent(MainActivity.this, login.class);
+            startActivity(i);
+
+        }
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterUser();
+            }
+        });
+
+    }
+
+    private void RegisterUser() {
+        String email, password;
+        email = edtemail.getText().toString();
+        password = editpassword.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent i = new Intent(MainActivity.this, login.class);
+                    startActivity(i);
+                    Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    // Registration failed
+                    Toast.makeText(getApplicationContext(), "Registration failed!!" + " Please try again later", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
+
+
